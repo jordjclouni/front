6bf42cc880036853d97135c9d21903874a411be6
arthur gaponik <pivoandreevic@gmail.com>
2025-05-12 10:59:38 +0300

    4
    4

--- src/config/JS_apiConfig.js
+++ src/config/JS_apiConfig.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000"; // Fallback для локальной разработки
+const API_URL = process.env.REACT_APP_API_URL || "https://back-production-08e5.up.railway.app"; // Fallback для локальной разработки
 
 export const API_TOPICS = `${API_URL}/api/topics`;
 export const API_SAFE_CELLS = `${API_URL}/api/safeshelves`;
